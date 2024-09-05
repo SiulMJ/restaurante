82ef40172e4d846b965733baed77b250abea3190
SiulMJ <luis.esgrima02@gmail.com>
2024-09-05 17:27:14 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="template")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/pedidos')
+def pedidos():
+    return render_template('pedidos.html')
+
+@app.route('/promociones')
+def promociones():
+    return render_template('promociones.html')
+
+@app.route('/ubicacion')
+def ubicacion():
+    return render_template('ubicacion.html')
+
+app.run(host="localhost", port=4000, debug=True)
